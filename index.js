c97de985cfb83d470d9519a43e6a536642d35736
Thuong181299 <thuong.nd174253@sis.hust.edu.vn>
2022-06-21 10:59:25 +0700

    Sua tieu de

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
+
+app.use(module("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
